6cf2ad02071b23ae6183d99d188706c751565ef5
DESKTOP-R00Q0A5\radud <radudragosflorian@gmail.com>
2024-11-24 18:44:50 +0200

    Modificare Test

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const SignUp = () => {
   return (
     <SafeAreaView>
-      <Text>Sign Up</Text>
+      <Text>Sign UUp</Text>
     </SafeAreaView>
   );
 };
