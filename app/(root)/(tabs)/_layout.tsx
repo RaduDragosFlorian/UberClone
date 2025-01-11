ca87da8b75e13b2cc1f60e5abf42040d76f1cf6f
DESKTOP-R00Q0A5\radud <radudragosflorian@gmail.com>
2025-01-11 22:05:25 +0200

    Update 11.01/3

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
-import { icons } from "@/constants";
 import { Tabs } from "expo-router";
-import { View, Image, ImageSourcePropType } from "react-native";
+import { Image, ImageSourcePropType, View } from "react-native";
+
+import { icons } from "@/constants";
 
 const TabIcon = ({
   source,
@@ -24,69 +25,71 @@ const TabIcon = ({
     </View>
   </View>
 );
-const Layout = () => (
-  <Tabs
-    initialRouteName="home"
-    screenOptions={{
-      tabBarActiveTintColor: "white",
-      tabBarInactiveTintColor: "white",
-      tabBarShowLabel: false,
-      tabBarStyle: {
-        backgroundColor: "#333333",
-        borderRadius: 50,
-        paddingBottom: 0,
-        overflow: "hidden",
-        marginHorizontal: 20,
-        marginBottom: 20,
-        height: 78,
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        flexDirection: "row",
-        position: "absolute",
-      },
-    }}
-  >
-    <Tabs.Screen
-      name="home"
-      options={{
-        title: "Home",
-        headerShown: false,
-        tabBarIcon: ({ focused }) => (
-          <TabIcon focused={focused} source={icons.home} />
-        ),
+
+export default function Layout() {
+  return (
+    <Tabs
+      initialRouteName="home"
+      screenOptions={{
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "white",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#333333",
+          borderRadius: 50,
+          paddingBottom: 0, // ios only
+          overflow: "hidden",
+          marginHorizontal: 20,
+          marginBottom: 20,
+          height: 78,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexDirection: "row",
+          position: "absolute",
+        },
       }}
-    />
-    <Tabs.Screen
-      name="rides"
-      options={{
-        title: "Rides",
-        headerShown: false,
-        tabBarIcon: ({ focused }) => (
-          <TabIcon focused={focused} source={icons.list} />
-        ),
-      }}
-    />
-    <Tabs.Screen
-      name="chat"
-      options={{
-        title: "Chat",
-        headerShown: false,
-        tabBarIcon: ({ focused }) => (
-          <TabIcon focused={focused} source={icons.chat} />
-        ),
-      }}
-    />
-    <Tabs.Screen
-      name="profile"
-      options={{
-        title: "Profile",
-        headerShown: false,
-        tabBarIcon: ({ focused }) => (
-          <TabIcon focused={focused} source={icons.profile} />
-        ),
-      }}
-    />
-  </Tabs>
-);
-export default Layout;
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.home} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="rides"
+        options={{
+          title: "Rides",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.list} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          title: "Chat",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.chat} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.profile} focused={focused} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
