e4b12aaf3e0eb53e61a13a49edfb98db8233c3c5
DESKTOP-R00Q0A5\radud <radudragosflorian@gmail.com>
2025-01-13 22:31:48 +0200

    Update 13.01/3

--- app/(api)/(stripe)/pay+api.ts
+++ app/(api)/(stripe)/pay+api.ts
@@ -9,10 +9,9 @@ export async function POST(request: Request) {
       body;
 
     if (!payment_method_id || !payment_intent_id || !customer_id) {
-      return new Response(
-        JSON.stringify({ error: "Missing required fields" }),
-        { status: 400 },
-      );
+      return new Response(JSON.stringify({ error: "Missing required field" }), {
+        status: 400,
+      });
     }
 
     const paymentMethod = await stripe.paymentMethods.attach(
